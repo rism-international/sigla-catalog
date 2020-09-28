0ee8fb57d5f9e907caed008aa2f88372d0360eb8
stephan <stephan@hirsch-online.com>
2020-09-28 09:19:33 +0200

    Build basic html template in js

--- sigla.js
+++ sigla.js
@@ -10,36 +10,60 @@ const nsMarc = "http://www.loc.gov/MARC21/slim";
 const nsZing = "http://www.loc.gov/zing/srw/";
 var results = document.querySelector('.siglaResultTables');
 var records = [];
+//Basic html template
+var markup = `
+      <div class="siglaQuery">
+        <div class="siglaQuerySelect">
+          <select id="siglaQuerySelect" name="advanced">
+            <option value="">Alle fields</option>
+            <option value="name:">Name</option>
+            <option value="sigla:">Library Sigla</option>
+            <option value="city:">City</option>
+            <option value="country:">Country</option>             
+          </select>
+        </div>
+        <div class="siglaQueryInput">
+          <input id="siglaQueryInput">
+        </div>
+        <div class="siglaQuerySubmit">
+          <input id="siglaQuerySubmit" type="submit" value="Search">
+        </div>
+      </div>
+      <div class="siglaResultSize">RISM: Hits 1-20 of <span id="resultSize"></span> for <span id="queryTerm" class="queryTerm"></span>.</div>
+      <div class="siglaResultTables"></div>
+`
+//Make an ajax request to SRU, build html and push records to results
+var search = function(){
+  document.querySelector("#siglaQuerySubmit").addEventListener("click", (e) => {
+    var queryTerm = document.querySelector("#siglaQueryInput").value;
+    var xhr = new XMLHttpRequest();
 
-document.querySelector("#siglaQuerySubmit").addEventListener("click", (e) => {
-  var queryTerm = document.querySelector("#siglaQueryInput").value;
-  var xhr = new XMLHttpRequest();
- 
-  // Ajax reuest to SRU  
-  xhr.onload = function () {
-    records = [];
-    if (xhr.status >= 200 && xhr.status < 300) {
-      parser = new DOMParser();
-      xmlDoc = parser.parseFromString(xhr.response, "text/xml");
-      var resultSize = xmlDoc.getElementsByTagNameNS(nsZing, "numberOfRecords")[0].innerHTML;
-      document.querySelector("#queryTerm").innerHTML = queryTerm;
-      document.querySelector("#resultSize").innerHTML = resultSize;
-      document.querySelector(".siglaResultSize").style.display = 'block';
-      var marcRecords = xmlDoc.getElementsByTagNameNS(nsMarc, "record");
-      for (let i = 0; i < marcRecords.length; i++) {
-        var record = buildRecord(marcRecords[i]);
-        records.push(record);
+    // Ajax reuest to SRU  
+    xhr.onload = function () {
+      records = [];
+      if (xhr.status >= 200 && xhr.status < 300) {
+        parser = new DOMParser();
+        xmlDoc = parser.parseFromString(xhr.response, "text/xml");
+        var resultSize = xmlDoc.getElementsByTagNameNS(nsZing, "numberOfRecords")[0].innerHTML;
+        document.querySelector("#queryTerm").innerHTML = queryTerm;
+        document.querySelector("#resultSize").innerHTML = resultSize;
+        document.querySelector(".siglaResultSize").style.display = 'block';
+        var marcRecords = xmlDoc.getElementsByTagNameNS(nsMarc, "record");
+        for (let i = 0; i < marcRecords.length; i++) {
+          var record = buildRecord(marcRecords[i]);
+          records.push(record);
+        }
+        createElements(records);
       }
-      createElements(records);
-    }
-    else {
-      console.log('The request failed!');
-    }
-    //outer scope
-  };
+      else {
+        console.log('The request failed!');
+      }
+      //outer scope
+    };
 
-  xhr.open('GET', `https://beta.rism.info/sru/institutions?operation=searchRetrieve&version=1.1&query=${queryTerm}%20AND%20librarySiglum=*-*&maximumRecords=20`);
-  xhr.send();
+    xhr.open('GET', `https://beta.rism.info/sru/institutions?operation=searchRetrieve&version=1.1&query=${queryTerm}%20AND%20librarySiglum=*-*&maximumRecords=20`);
+    xhr.send();
+  });
 
   //Function to add various div-tags to document.parent .siglaResultTables
   var createElements = function(collection){
@@ -84,16 +108,20 @@ document.querySelector("#siglaQuerySubmit").addEventListener("click", (e) => {
     }
     return record;
   }
+}
+
+
+window.onload = function() {
+  document.querySelector("#siglaCatalog").innerHTML = markup;
+  search();
+}
 
 
 
-});
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
